847ada8772fa611b1a618022dbd5857f494a5ba6
William L <w-lindvall@outlook.com>
2020-05-23 02:45:40 -0700

    add welcome text

--- www/pages/index.js
+++ www/pages/index.js
@@ -15,8 +15,15 @@ const Home = ({boards}) => (
     <h1 className="visibly-hidden">Shinme Network</h1>
     <Nav className="site__nav" boards={boards} />
 
-    <main className="site__main -center">
-      <span role="img" aria-label="seedling">
+    <main className="site__main">
+      <p className="site__welcome">
+        Welcome to shinme.network, an open image- and text-board. Please post
+        any requests in the <a href="/board/requests">requests board</a> and
+        submit any issues or improvements you would like to see{' '}
+        <a href="https://github.com/shwilliam/shinme-network/issues">here</a>.
+      </p>
+
+      <span className="sprout" role="img" aria-label="">
         🌱
       </span>
     </main>
